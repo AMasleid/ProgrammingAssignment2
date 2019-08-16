911b5a621d19dea47924ba4588f01897be7b04ae
KAN03584 <Alex.Masleid@cslbehring.com>
2019-08-16 15:40:11 -0500

    Signed-off-by: KAN03584 <Alex.Masleid@cslbehring.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Stores and retrieves cached matrix data and results 
+## for solve function
 
-## Write a short comment describing this function
+## Cashes matrix data and calculations
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Checks for cached calculations and either solves matrix 
+## or retrieves cached results with a message
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
